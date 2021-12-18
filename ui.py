02840064f740c6301e65eb6d5c362ff2e7605587
kinderror <87951557+kinderror@users.noreply.github.com>
2021-12-18 19:55:51 +0300

    Update ui.py

--- ui.py
+++ ui.py
@@ -3,30 +3,57 @@ import pygame
 import sys
 from constants import WIDTH, HEIGHT
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-screen = pygame.display.set_mode((WIDTH + 150, HEIGHT))  # TODO: допокно для кнопок и регуляторов, нужно его улучшить и сделать красивым
+NUMBER_OF_TYPES = 3
+NODE_COUNT = 250
+SIMULATIONS_PER_FRAME = 2
 
-NUMBER_OF_TYPES = 4  # TODO: добавить контроллер-scrollbar
-NODE_COUNT = 300  # TODO: добавить контроллер-scrollbar
-SIMULATIONS_PER_FRAME = 2  # TODO: добавить контроллер-scrollbar
+DRAW_CONNECTIONS = True
+type_for_click = 0
 
-DRAW_CONNECTIONS = True  # TODO: добавить галочку
-type_for_click = 0  # TODO: заготовочка под выбор типа частицы для создания по клику
+color_light = (170, 170, 170)
+color_dark = (120, 120, 120)
 
-color_light = (250, 250, 250)
-color_dark = (170, 170, 170)
-text = ''
-
-
-def create_button():
-    global text
-    smallfont = pygame.font.SysFont('Corbel', int(WIDTH / 40))
-    text = smallfont.render('Create new world', True, (0, 0, 0))  # TODO: сделать кнопку красивой
+controllers = {'force': {'x': 70, 'y': 123}, 'radius': {'x': 70, 'y': 153},
+               'speed': {'x': 70, 'y': 183}, 'frame': {'x': 70, 'y': 213},
+               'connections': True, 'types': {'x': 70, 'y': 295},
+               'count': {'x': 70, 'y': 325}, 'brush': 0}
 
 
-def draw_button(mouse):
-    if mouse[0] >= WIDTH and mouse[1] <= HEIGHT / 5:
-        pygame.draw.rect(screen, color_light, [WIDTH, 0, WIDTH + 150, HEIGHT / 5])
+def draw_buttons(mouse):
+    if 30 <= mouse[0] <= 250 and 347 <= mouse[1] <= 387:
+        pygame.draw.rect(screen, color_light, [30, 347, 220, 40])
     else:
-        pygame.draw.rect(screen, color_dark, [WIDTH, 0, WIDTH + 150, HEIGHT / 5])
-    screen.blit(text, (WIDTH + 20, HEIGHT / 10 - 5))
+        pygame.draw.rect(screen, color_dark, [30, 347, 220, 40])
+    if 30 <= mouse[0] <= 250 and 40 <= mouse[1] <= 80:
+        pygame.draw.rect(screen, color_light, [30, 40, 220, 40])
+    else:
+        pygame.draw.rect(screen, color_dark, [30, 40, 220, 40])
+
+
+def controllers_info(mouse):
+    x, y = mouse[0], mouse[1]
+    for s in controllers:
+        if not (s == 'connections' or s == 'brush'):
+            if controllers[s]['y'] - 3 <= y <= controllers[s]['y'] + 9:
+                controllers[s]['x'] = x - 4
+        if s == 'connections':
+            if 30 <= x <= 50 and 230 <= y <= 250:
+                if controllers[s]:
+                    controllers[s] = False
+                else:
+                    controllers[s] = True
+    controllers['frame']['x'] = int((controllers['frame']['x'] - 26) / 24) * 24 + 26
+    controllers['types']['x'] = int((controllers['types']['x'] - 26) / 27) * 27 + 26
+
+
+def draw_controllers():
+    for s in controllers:
+        if not (s == 'connections' or s == 'brush'):
+            pygame.draw.rect(screen, (150, 150, 150), [30, controllers[s]['y'], 220, 6])
+            pygame.draw.rect(screen, (100, 100, 100), [controllers[s]['x'], controllers[s]['y'] - 3, 8, 12])
+        if s == 'connections':
+            pygame.draw.rect(screen, color_dark, [30, 230, 20, 20])
+            if controllers[s]:
+                pygame.draw.polygon(screen, 'white', [(36, 250), (30, 244), (32, 242), (36, 246), (48, 230), (50, 232)])
