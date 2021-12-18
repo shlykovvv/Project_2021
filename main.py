ed469baf4830be6a5293a49f9ee0021c2dc151aa
kinderror <87951557+kinderror@users.noreply.github.com>
2021-12-18 19:53:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
 import pygame.draw
 import model
-from constants import WIDTH, HEIGHT, FPS
+from constants import WIDTH, HEIGHT, FPS, BACKGROUND_COLOR
 import view
 import ui
-
 
 model.create_new_world()
 
@@ -12,7 +11,7 @@ pygame.init()
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-ui.create_button()
+opened = True
 
 while not finished:
     clock.tick(FPS)
@@ -22,17 +21,27 @@ while not finished:
             finished = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             if mouse[0] <= WIDTH and mouse[1] <= HEIGHT:
-                model.Particle(ui.type_for_click, mouse[0], mouse[1])
-            if mouse[0] >= WIDTH and mouse[1] <= HEIGHT / 5:
-                model.create_new_world()
-
+                model.add_particle(ui.type_for_click, mouse[0], mouse[1])
+            if 30 <= mouse[0] <= 250:
+                ui.controllers_info(mouse)
+                if 40 <= mouse[1] <= 80 and opened:
+                    opened = False
+                if 347 <= mouse[1] <= 387:
+                    model.create_new_world()
+            if 20 <= mouse[0] <= 40 and 20 <= mouse[1] <= 40 and not opened:
+                opened = True
+    ui.screen.fill(BACKGROUND_COLOR)
     for i in range(ui.SIMULATIONS_PER_FRAME):
         model.mmmodel()
-    view.draw_background()
     view.draw_particles(model.fields)
     if ui.DRAW_CONNECTIONS:
         view.draw_links(model.links)
-
-    ui.draw_button(mouse)
+    if opened:
+        view.draw_settings()
+        ui.draw_controllers()
+        ui.draw_buttons(mouse)
+        view.print_text()
+    else:
+        view.draw_closed_settings()
     pygame.display.update()
 pygame.quit()
