0bef354e204d9308d78897912f07591eed7dccdf
kinderror <87951557+kinderror@users.noreply.github.com>
2021-12-19 04:59:52 +0300

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,6 +1,4 @@
-import pygame.draw
 import pygame
-import math
 import sys
 from constants import WIDTH, HEIGHT
 
@@ -10,59 +8,44 @@ NUMBER_OF_TYPES = 3
 NODE_COUNT = 250
 SIMULATIONS_PER_FRAME = 2
 
-NODE_RADIUS = 5  # TODO: моё мнение, что при нехватки времени контроллеры этих параметров можно и убрать, положив переменные обратно к константам
-LINK_FORCE = - 0.015  # TODO: моё мнение, что при нехватки времени контроллеры этих параметров можно и убрать, положив переменные обратно к конста
-SPEED = 4  # TODO: моё мнение, что при нехватки времени контроллеры этих параметров можно и убрать, положив переменные обратно к конста
+NODE_RADIUS = 5
+LINK_FORCE = - 0.015
+SPEED = 4
 
-# DRAW_CONNECTIONS = True
 type_for_click = 0
 
 color_light = (170, 170, 170)
 color_dark = (120, 120, 120)
 
-controllers = {'force': {'x': 70, 'y': 123}, 'radius': {'x': 70, 'y': 153},
-               'speed': {'x': 70, 'y': 183}, 'frame': {'x': 70, 'y': 213},
-               'connections': True, 'types': {'x': 70, 'y': 295},
-               'count': {'x': 70, 'y': 325}, 'brush': 0}
+controllers = {'force': {'x': 26 - (LINK_FORCE / 0.04) * 220, 'y': 123},
+               'radius': {'x': 26 + ((NODE_RADIUS - 3) / 7) * 220, 'y': 153},
+               'speed': {'x': 26 + (SPEED / 40) * 220, 'y': 183},
+               'frame': {'x': 26 + (SIMULATIONS_PER_FRAME - 1) * 24, 'y': 213},
+               'types': {'x': 26 + (NUMBER_OF_TYPES - 1) * 27, 'y': 295},
+               'count': {'x': 26 + (NODE_COUNT / 300) * 220, 'y': 325},
+               'connections': True, 'brush': 0}
 
 
-def draw_buttons(mouse):
-    if 30 <= mouse[0] <= 250 and 347 <= mouse[1] <= 387:
-        pygame.draw.rect(screen, color_light, [30, 347, 220, 40])
-    else:
-        pygame.draw.rect(screen, color_dark, [30, 347, 220, 40])
-    if 30 <= mouse[0] <= 250 and 40 <= mouse[1] <= 80:
-        pygame.draw.rect(screen, color_light, [30, 40, 220, 40])
-    else:
-        pygame.draw.rect(screen, color_dark, [30, 40, 220, 40])
-
-
-def controllers_info(mouse):
-    global NODE_COUNT
+def change_controllers(mouse):
     x, y = mouse[0], mouse[1]
     for s in controllers:
         if s != 'connections' and s != 'brush':
             if controllers[s]['y'] - 3 <= y <= controllers[s]['y'] + 9:
                 controllers[s]['x'] = x - 4
-                if s == 'count':
-                    NODE_COUNT = math.floor((controllers[s]['x'] - 26) / 190 * 600)
-                    print('ddd')
         if s == 'connections':
             if 30 <= x <= 50 and 230 <= y <= 250:
                 if controllers[s]:
                     controllers[s] = False
                 else:
                     controllers[s] = True
-    controllers['frame']['x'] = int((controllers['frame']['x'] - 26) / 24) * 24 + 26
+    controllers['frame']['x'] = ((controllers['frame']['x'] - 26) // 24) * 24 + 26
     controllers['types']['x'] = int((controllers['types']['x'] - 26) / 27) * 27 + 26
 
 
-def draw_controllers():
-    for s in controllers:
-        if s != 'connections' and s != 'brush':
-            pygame.draw.rect(screen, (150, 150, 150), [30, controllers[s]['y'], 220, 6])
-            pygame.draw.rect(screen, (100, 100, 100), [controllers[s]['x'], controllers[s]['y'] - 3, 8, 12])
-        if s == 'connections':
-            pygame.draw.rect(screen, color_dark, [30, 230, 20, 20])
-            if controllers[s]:
-                pygame.draw.polygon(screen, 'white', [(36, 250), (30, 244), (32, 242), (36, 246), (48, 230), (50, 232)])
+def change_characteristics():
+    global NODE_COUNT, LINK_FORCE, NODE_RADIUS, SPEED, SIMULATIONS_PER_FRAME
+    LINK_FORCE = - int((controllers['force']['x'] - 26) / 220 * 40) / 1000
+    NODE_COUNT = int((controllers['count']['x'] - 26) / 220 * 300)
+    NODE_RADIUS = int(300 + (controllers['radius']['x'] - 26) / 220 * 700) / 100
+    SPEED = int((controllers['speed']['x'] - 26) / 220 * 400) / 10
+    SIMULATIONS_PER_FRAME = int(1 + (controllers['frame']['x'] - 2) / 220 * 9)
